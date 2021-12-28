21616027d0722372c868ec91dce43588825432c9
Rade1210 <39063708+Rade1210@users.noreply.github.com>
2021-12-28 19:55:11 +0100

    Created Api Client Class

--- app/src/main/java/com/example/tvshowstracker/activities/MainActivity.java
+++ app/src/main/java/com/example/tvshowstracker/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.tvshowstracker;
+package com.example.tvshowstracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.tvshowstracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
