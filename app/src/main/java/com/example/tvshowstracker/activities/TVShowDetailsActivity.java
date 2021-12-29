cab18cab6bd817dd04c2da11d216658640644515
Rade1210 <39063708+Rade1210@users.noreply.github.com>
2021-12-29 15:03:19 +0100

    Added functionality to display url when we click on a show

--- app/src/main/java/com/example/tvshowstracker/activities/TVShowDetailsActivity.java
+++ app/src/main/java/com/example/tvshowstracker/activities/TVShowDetailsActivity.java
@@ -1,18 +1,43 @@
 package com.example.tvshowstracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.tvshowstracker.R;
+import com.example.tvshowstracker.databinding.ActivityTvshowDetailsBinding;
+import com.example.tvshowstracker.viewmodels.TVShowDetailsViewModel;
 
 public class TVShowDetailsActivity extends AppCompatActivity {
+
+    private ActivityTvshowDetailsBinding activityTvshowDetailsBinding;
+    private TVShowDetailsViewModel tvShowDetailsViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tvshow_details);
+        activityTvshowDetailsBinding = DataBindingUtil.setContentView(this, R.layout.activity_tvshow_details);
+        doInitialization();
     }
 
+    private void doInitialization(){
+        tvShowDetailsViewModel = new ViewModelProvider(this).get(TVShowDetailsViewModel.class);
+        getTVShowDetails();
+    }
+
+    private void getTVShowDetails(){
+        activityTvshowDetailsBinding.setIsLoading(true);
+        String tvShowId = String.valueOf(getIntent().getIntExtra("id", -1));
+        tvShowDetailsViewModel.getTVShowDetails(tvShowId).observe(
+                this, tvShowDetailsResponse -> {
+                    activityTvshowDetailsBinding.setIsLoading(false);
+                    Toast.makeText(getApplicationContext(), tvShowDetailsResponse.getTvShowDetails().getUrl(), Toast.LENGTH_SHORT).show();
+                }
+        );
+    }
 
 }
