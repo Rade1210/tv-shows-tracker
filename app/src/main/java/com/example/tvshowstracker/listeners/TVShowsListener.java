9ee437a6d05e276596896df29c64d5f9c29045ee
Rade1210 <39063708+Rade1210@users.noreply.github.com>
2021-12-29 14:42:04 +0100

    Created Interface listener

---
+++ app/src/main/java/com/example/tvshowstracker/listeners/TVShowsListener.java
@@ -0,0 +1,9 @@
+package com.example.tvshowstracker.listeners;
+
+import com.example.tvshowstracker.models.TVShow;
+
+public interface TVShowsListener {
+
+   void onTVShowClicked(TVShow tvShow);
+
+}
